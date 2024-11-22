8be0d7d2fc8824a1dba2a3cfe759841b772c1c16
Rishabh Shankar <rishabhshankar05@gmail.com>
2024-11-22 23:53:36 +0530

    Layout setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
+import { Routes, Route } from 'react-router-dom';
+
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
+import { Home } from './_root/pages';
 import './globals.css';
 const App = () => {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <main className='flex h-screen'>
+            <Routes>
+                {/* Public Routes - anyone can access*/}
+
+                <Route element={<AuthLayout />}>
+                    <Route path='/sign-in' element={<SigninForm />} />
+                    <Route path='/sign-in' element={<SignupForm />} />
+                </Route>
+
+                {/* Private Routes - only signed in users can access*/}
+
+                <Route element={<RootLayout />}>
+                    <Route index element={<Home />} />
+                </Route>
+
+            </Routes>
+        </main>
     )
 }
 
