8f8f1b67128c529591d41b57f3732f16b917fe35
Rishabh Shankar <rishabhshankar05@gmail.com>
2024-11-23 21:20:39 +0530

    pages

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import Bottombar from "@/components/shared/Bottombar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Topbar from "@/components/shared/Topbar";
+import { Outlet } from "react-router-dom";
 
-const RootLayout = () => {
+const Home = () => {
     return (
-        <div>RootLayout</div>
-    )
-}
+        <div className="w-full md:flex">
+            <Topbar />
+            <LeftSidebar />
 
-export default RootLayout
+            <section className="flex flex-1 h-full">
+                <Outlet />
+            </section>
+
+            <RightSidebar />
+
+            <Bottombar />
+        </div>
+    );
+};
+
+export default Home;
--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -4,6 +4,20 @@ export const SignupValidation = z.object({
     name: z.string().min(2, { message: "Name must be atleast 2 characters!" }),
     username: z.string().min(4, { message: "Username must be atleast 4 characters!" }),
     email: z.string().email(),
-    password: z.string().min(8, { message: "Password must be atleast 8 characters!" }),
+    password: z.string().min(8, { message: "Password must be at least 8 characters!" }),
 
-})
+});
+
+export const SigninValidation = z.object({
+    email: z.string().email(),
+    password: z
+        .string()
+        .min(8, { message: "Password must be at least 8 characters!" }),
+});
+
+export const PostValidation = z.object({
+    caption: z.string().min(5).max(2200),
+    file: z.custom<File[]>(),
+    location: z.string().min(2).max(100),
+    tags: z.string(),
+});
