4ef1914e9028af922a588ee26d81a885e75da7cc
Rishabh Shankar <rishabhshankar05@gmail.com>
2024-11-23 20:58:25 +0530

    shared components added

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,51 +1,101 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { Button } from "@/components/ui/button"
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage, } from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+import { toast } from "@/components/ui/use-toast";
+import { Button } from "@/components/ui/button";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { SignupValidation } from "@/lib/validation";
+import { z } from "zod";
+import Loader from "@/components/shared/Loader";
+import { Link, useNavigate } from "react-router-dom";
 
-import React from 'react'
-import { SignupValidation } from "@/lib/validation"
-import { z } from "zod"
-import Loader from "@/components/shared/Loader"
-import { Link } from "react-router-dom"
-import { createUserAccount } from "@/lib/appwrite/api"
+import {
+    useCreateUserAccount,
+    useSignInAccount,
+} from "@/lib/react-query/queriesAndMutations";
 
+import { useUserContext } from "@/context/AuthContext";
 
 const SignupForm = () => {
-    const isLoading = false;
+    const { checkAuthUser, isLoading: isUserLoading } = useUserContext();
+
+    const navigate = useNavigate();
+
+    const { mutateAsync: createUserAccount, isPending: isCreatingUser } =
+        useCreateUserAccount();
+
+    const { mutateAsync: signInAccount, isPending: isSigningIn } =
+        useSignInAccount();
+
     // 1. Define your form.
     const form = useForm<z.infer<typeof SignupValidation>>({
         resolver: zodResolver(SignupValidation),
         defaultValues: {
-            name: '',
-            username: '',
-            email: '',
-            password: '',
+            name: "",
+            username: "",
+            email: "",
+            password: "",
         },
-    })
+    });
 
     // 2. Define a submit handler.
     async function onSubmit(values: z.infer<typeof SignupValidation>) {
         const newUser = await createUserAccount(values);
-        console.log(newUser);
-    }
 
+        if (!newUser) {
+            toast({
+                title: "Sign up failed. Please try again.",
+            });
+            return;
+        }
+
+        const session = await signInAccount({
+            email: values.email,
+            password: values.password,
+        });
+
+        if (!session) {
+            toast({
+                title: "Sign in failed. Please try again.",
+                variant: "destructive",
+            });
+            return;
+        }
+
+        const isLoggedIn = await checkAuthUser();
+
+        if (isLoggedIn) {
+            form.reset();
+            navigate("/");
+        } else {
+            toast({
+                title: "Sign up failed. Please try again.",
+                variant: "destructive",
+            });
+            return;
+        }
+    }
     return (
         <Form {...form}>
-
             <div className="sm:w-420 flex-center flex-col">
-                <img src="/assets/images/Snapgram.svg" alt="logo" />
-
-                <h2 className="h3-bold md:h2-bold pt-1 sm:pt-1">Create a new account</h2>
+                <img src="/assets/images/logo.svg" alt="logo" />
+                <h2 className="h3-bold md:h2-bold pt-2 sm:pt-2">
+                    Create a new account
+                </h2>
                 <p className="text-light-3 small-medium md:base-regular mt-2">
-                    To use SnapDrop, please enter your details!
+                    To use snapgram, please enter your account details
                 </p>
-
-
-
-                <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="flex flex-col gap-5 w-full mt-4">
                     <FormField
                         control={form.control}
                         name="name"
@@ -59,6 +109,7 @@ const SignupForm = () => {
                             </FormItem>
                         )}
                     />
+
                     <FormField
                         control={form.control}
                         name="username"
@@ -72,6 +123,7 @@ const SignupForm = () => {
                             </FormItem>
                         )}
                     />
+
                     <FormField
                         control={form.control}
                         name="email"
@@ -85,6 +137,7 @@ const SignupForm = () => {
                             </FormItem>
                         )}
                     />
+
                     <FormField
                         control={form.control}
                         name="password"
@@ -99,23 +152,26 @@ const SignupForm = () => {
                         )}
                     />
                     <Button type="submit" className="shad-button_primary">
-                        {isLoading ? (
-                            <div className="flex-center gap-2">
-                                <Loader />
-                                Loading...
+                        {isCreatingUser || isSigningIn || isUserLoading ? (
+                            <div className="flex center gap-2">
+                                <Loader /> Loading...
                             </div>
-                        ) : "Sign up"}
+                        ) : (
+                            "Sign up"
+                        )}
                     </Button>
-
                     <p className="text-small-regular text-light-2 text-center mt-2">
                         Already have an account?
-                        <Link to="/sign-in" className="text-primary-500 text-small-semibold ml-1"> Log in </Link>
+                        <Link
+                            to="/sign-in"
+                            className="text-primary-500 ml-1 text-small-semibold">
+                            Log in
+                        </Link>
                     </p>
                 </form>
             </div>
         </Form>
+    );
+};
 
-    )
-}
-
-export default SignupForm
+export default SignupForm;
